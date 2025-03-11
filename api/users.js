b9db3f8a132550a262ae95651df588b68d0c505c
Jamie-Lee Gordon <gordonjamie123@yahoo.co.uk>
2025-03-11 17:16:21 +0000

    first

--- api/users.js
+++ api/users.js
@@ -1,9 +1,19 @@
 module.exports = (req, res) => {
+    // Set the CORS headers to allow requests from localhost
+    res.setHeader('Access-Control-Allow-Origin', '*'); // or use 'http://localhost:3001' to restrict to only your local dev
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // If it's a pre-flight request (OPTIONS request), we just respond with status 200
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method == 'GET') {
         res.json([
-            {name: "efjhiefiejfioee"}
-        ])
+            { name: "efjhiefiejfioee" }
+        ]);
     } else {
-
+        res.status(405).send('Method Not Allowed');
     }
-}
+};
